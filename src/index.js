4cd06dfc239a4dd344beae37767e9dcfaedbafff
Courtney Phillips <courtney.mae.phillips@gmail.com>
2018-03-05 13:49:01 -0800

    Move entry point into sourcecode directory.

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+import { AppContainer } from 'react-hot-loader';
+
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component/>
+    </AppContainer>,
+    document.getElementById('react-app-root')
+  );
+};
+
+render(App);
