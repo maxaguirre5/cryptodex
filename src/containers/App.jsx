84b88223c71856e6bae0d492441199de10370e61
Courtney Phillips <courtney.mae.phillips@gmail.com>
2018-03-05 14:40:34 -0800

    Fix linting errors.

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import Header from './../components/Header';
+import React from 'react'
+import Header from './../components/Header'
 
 const App = () => {
-  return (
-    <div>
-      <Header/>
+    return (
+        <div>
+            <Header/>
       Yo, it's the parent compnent.
-    </div>
-  );
-};
+        </div>
+    )
+}
 
-export default App; 
+export default App 
